a14d11d10bac9ddfe00bea81e3d31a0b5d8f6dc8
Grainne <readyg@tcd.ie>
2022-06-04 05:32:22 +0100

    .

--- Days/Day 2/Project - Tip Calculator.py
+++ Days/Day 2/Project - Tip Calculator.py
@@ -17,6 +17,7 @@
 # Example Output:
 #   Each person should pay: $19.93
 # Personal Notes:
+
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
