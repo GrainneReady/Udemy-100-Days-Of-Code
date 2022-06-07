4d12e182e9b8f03e6f9a88b3cb243c194e763eb5
Grainne <readyg@tcd.ie>
2022-06-07 05:20:35 +0100

    For Loops, Range and Code Blocks

--- Days/Day 5/Project - Password Generator.py
+++ Days/Day 5/Project - Password Generator.py
@@ -5,6 +5,8 @@
 # Instructions:
 #   None given. Create a password generator.
 # Personal Notes:
+# random.shuffle() will shuffle the order of a list, this could also be done by creating a new list, choosing an index from the old list at random, adding the element
+#   at the chosen index to the new list, and removing said element from the old list until there are no remaining elements in the old list.
 
 #Password Generator Project
 import random
@@ -20,26 +22,21 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 # This will generate a password where the order is not randomised
-unrandomisedPassword = []
+password = []
 generatedCharacter = ''
 for letter in range(0, nr_letters):
     generatedCharacter = letters[random.randint(0, len(letters)-1)]
-    unrandomisedPassword.append(generatedCharacter)
+    password.append(generatedCharacter)
 for symbol in range(0, nr_symbols):
     generatedCharacter = symbols[random.randint(0, len(symbols) - 1)]
-    unrandomisedPassword.append(generatedCharacter)
+    password.append(generatedCharacter)
 for number in range(0, nr_numbers):
     generatedCharacter = numbers[random.randint(0, len(numbers) - 1)]
-    unrandomisedPassword.append(generatedCharacter)
-#print(unrandomisedPassword)
+    password.append(generatedCharacter)
+print(password)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 # This will then randomize the order of the password that was previously generated
-indexFromUnrandomised = 0
-randomisedPassword = []
-for character in range(0, len(unrandomisedPassword) - 1):
-    indexFromUnrandomised = random.randint(0, len(unrandomisedPassword) - 1)
-    randomisedPassword.append(unrandomisedPassword[indexFromUnrandomised])
-    del unrandomisedPassword[indexFromUnrandomised]
-print(randomisedPassword)
+random.shuffle(password)
+print(password)
