0aa79995eaa12e711fe15e5d5ff5fb349bf70be3
Grainne <readyg@tcd.ie>
2022-06-08 09:52:36 +0100

    Functions, Code Blocks and While Loops

--- Days/Day 6/Project - Escaping the Maze.py
+++ Days/Day 6/Project - Escaping the Maze.py
@@ -34,7 +34,7 @@
 #    if right_is_clear():
 #        turn_right()
 #        move()
-#    elif not right_is_clear() and front_is_clear():
+#    elif front_is_clear():
 #        move()
 #    else:
 #        turn_left()
