b2d901ea031c1eca02eeb831382769867bf578e3
Grainne <readyg@tcd.ie>
2022-06-20 23:04:42 +0100

    Working with Variables to Manage Data

---
+++ Days/Day 01/Exercise 3 - Input Function copy.py
@@ -0,0 +1,17 @@
+# This program is a direct copy of a program I made following Udemy's Day 1 of 100 Days of Code: The Complete Python Pro Bootcamp for 2022
+#   https://www.udemy.com/course/100-days-of-code/
+# Assignment Link:
+#   https://app.codingrooms.com/management/assignments/364788/overview
+# Instructions:
+#   Write a program that prints the number of characters in a user's name. You might need to Google for a function 
+#     that calculates the length of a string.
+# Example Input:
+#   Angela
+# Example Output:
+#   6
+# Submission Result (100%):
+#   https://i.gyazo.com/6a9ae5bbc1f09498f1aefcf418e5ec58.png
+# Personal Notes:
+#   Python len(x) function returns the length of string x, aka, the number of characters in string x
+
+print(len(input("What is your name?")))
---
+++ Days/Day 01/Project - Band Name Generator.py
@@ -0,0 +1,19 @@
+# This program is made following Udemy's Day 1 of 100 Days of Code: The Complete Python Pro Bootcamp for 2022
+#      https://www.udemy.com/course/100-days-of-code/
+# Preview URL:
+#      https://www.replit.com/@appbrewery/band-name-generator-end
+
+#1. Create a greeting for your program
+print("Welcome to the Band Name Generator")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet
+pet = input("What's your pet's name?\n")
+
+#4. Combine the name of their city and pet and show them their band name
+print("Your band name could be " + city + " " + pet)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
